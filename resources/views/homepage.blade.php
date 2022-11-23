aa2f81e0cc8f59d1bab54e3ad42515e0ce18a17b
Rangga Adithia <rangga.adithia26@gmail.com>
2022-11-23 16:09:48 +0800

    Setup Laravel,Vite,Tailwindcss

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.index')
+
+@section('container')
+    <h1 class="text-2xl">Hello World</h1>
+@endsection
---
+++ resources/views/layouts/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>My Tour Bali</title>
+  @vite('resources/css/app.css')
+</head>
+<body>
+  @yield('container')
+</body>
+</html>
