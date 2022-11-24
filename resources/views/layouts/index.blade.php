a3a763dc3c9cea1ea53156f8432275712ccef85d
Rangga Adithia <rangga.adithia26@gmail.com>
2022-11-24 22:03:23 +0800

    import font

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -5,9 +5,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>My Tour Bali</title>
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=K2D:wght@300;400;600;700&family=Kaushan+Script&family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
   @vite('resources/css/app.css')
 </head>
 <body>
