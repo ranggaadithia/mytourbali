76e6898e38e006d89c3e3ad14a531134d8028c4b
Rangga Adithia <rangga.adithia26@gmail.com>
2022-11-27 11:23:33 +0800

    create homepage

--- resources/views/web/homepage.blade.php
+++ resources/views/web/homepage.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.index')
 
 @section('container')
-    <h1 class="text-2xl font-kaushan font-normal">Hallo</h1>
-    <h1 class="text-2xl font-subtitle">Hallo</h1>
+  {{-- hero section --}}
+  <section id="hero" class="hero">
+    <img src="/img" alt="">
+  </section>
+  {{-- end hero --}}
 @endsection
