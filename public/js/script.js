afb1967050b19a585080638cb85bcb5d624afa8d
Rangga Adithia <rangga.adithia26@gmail.com>
2022-12-14 10:48:01 +0800

    add slicky in testimonial

--- public/js/script.js
+++ public/js/script.js
@@ -14,3 +14,33 @@ close.addEventListener("click", function() {
   modal.classList.toggle("hidden");
 });
 
+$(document).ready(function(){
+  $('.testi-card').slick({
+  arrows:false,
+  infinite:true,
+  centerMode: true,
+  centerPadding: '40px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '10px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+  
+});
+});
--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -7,11 +7,16 @@
   <title>My Tour Bali</title>
   @vite('resources/css/app.css')
   <link rel="stylesheet" href="icofont/icofont.min.css">
+  <link rel="stylesheet" href="assets/slick/slick.css">
+  <link rel="stylesheet" href="assets/slick/slick-theme.css">
 </head>
 <body class="font-poppins">
   @yield('container')
 
   @vite('resources/js/index.min.js')
+  <script src="https://code.jquery.com/jquery-3.6.2.min.js" integrity="sha256-2krYZKh//PcchRtd+H+VyyQoZ/e3EcrkxhM8ycwASPA=" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-migrate-3.4.0.min.js" integrity="sha256-mBCu5+bVfYzOqpYyK4jm30ZxAZRomuErKEFJFIyrwvM=" crossorigin="anonymous"></script>
+  <script src="assets/slick/slick.min.js"></script>
   <script src="js/script.js"></script>
 </body>
 </html>
