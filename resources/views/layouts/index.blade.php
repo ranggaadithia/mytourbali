c4b41029d5212e42e05c4a984db94c98267d9ebb
Rangga Adithia <rangga.adithia26@gmail.com>
2024-12-07 15:15:21 +0800

    feat: update sitemap with new URLs and last modified dates; add Google Tag Manager scripts to layout

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -1,6 +1,13 @@
 <!DOCTYPE html>
 <html lang="en" class="scroll-smooth">
 <head>
+  <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-PBS9RMDF');</script>
+  <!-- End Google Tag Manager -->
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
@@ -19,6 +26,16 @@
 
     gtag('config', 'G-QP8LRLDLPE');
   </script>
+
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-16747585783"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-16747585783');
+</script>
 
   {{-- SEO --}}
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
